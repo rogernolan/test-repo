601bf005e572fdada68d04a1fd79c977f2c952c4
rog <rog@hatbat.net>
2011-12-07 18:18:08 +0000

    Shiny new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToDoServer::Application.config.secret_token = 'c3285e73938ffc6ae37c9396c56d488f5141ac9217a515c6c8fb40d49a90c1995678775accddad67710b45fa9a66d6e66bf331281c17db530b1f1777b12edd79'
